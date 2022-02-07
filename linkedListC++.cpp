03d0b5317e8cd3a7f9f36f02dc98f15e947ae241
centaurusghost <69569419+centaurusghost@users.noreply.github.com>
2022-02-07 15:36:46 +0545

    Add files via upload

--- linkedListC++.cpp
+++ linkedListC++.cpp
@@ -1,47 +1,54 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
-class linkedList{
-	private: char *error="Error\n";
-	public:
-		int data; linkedList *node;
-	    	linkedList *createNode(int data1){
-		   linkedList *temp;
-		   try{
-		   	temp = new linkedList;
-		   	if(temp){
-		   		temp->data=data1;
-			temp->node=NULL;
-			return temp;
-			   }
-			   else{
-			   	throw error;
-			   }
-		   }
-		   catch(char *err){
-		   	cout<<"Cannot create Node"<<endl;
-		   }
-			
-		}
-		
-		void printList(linkedList *temp){
-			while(temp!=NULL){
-				cout<<temp->data<<endl;
-				temp=temp->node;
-			}
-		}
-	inline	linkedList *setNode(linkedList *temp){
-			return temp->node;
-		}
+class linkedList {
+  public:
+    int data;
+  linkedList * node;
+  friend class driver;
 };
-int main(){
-	linkedList *node, *temp,*start;
-	node = node->createNode(11);
-    start = node;
-	for(int i=1; i<=3; i++){
-		node->node=node->createNode(i*12);
-		node = node->setNode(node);	                 
-	}
-	temp=start;
-	node->printList(temp);
-	
-}                  
+class driver_Class {
+  public:
+    char * error = "error\n";
+  linkedList * createNode(int);
+  void printList(linkedList * temp) {
+    while (temp != NULL) {
+      cout << temp -> data << endl;
+      temp = temp -> node;
+    }
+  }
+  inline linkedList * setNode(linkedList * temp) {
+    return temp -> node;
+  }
+  void driver_Class::setNumber(linkedList * node, int n, driver_Class & d1) {
+    for (int i = 1; i <= n; i++) {
+      node -> node = d1.createNode(i * 12);
+      node = d1.setNode(node);
+    }
+  }
+};
+linkedList * driver_Class::createNode(int data1) {
+  linkedList * temp;
+  try {
+    temp = new linkedList;
+    if (temp) {
+      temp -> data = data1;
+      temp -> node = NULL;
+      return temp;
+    } else {
+      throw error;
+    }
+  } catch (char * err) {
+    cout << "Cannot create Node" << endl;
+  }
+}
+
+int main() {
+  driver_Class d1;
+  linkedList * node, * start, * temp;
+  node = d1.createNode(11);
+  start = node;
+  d1.setNumber(node, 10, d1);
+  temp = start;
+  d1.printList(temp);
+}
