5d0df1b30f369c21a39fd51ffa2b7fc81beff47f
centaurusghost <69569419+centaurusghost@users.noreply.github.com>
2022-02-08 09:26:28 +0545

    Update linkedListC++.cpp

--- linkedListC++.cpp
+++ linkedListC++.cpp
@@ -1,36 +1,70 @@
 #include <iostream>
 
 using namespace std;
-class linkedList {
+class LinkedList {
   private:
     int data;
-  linkedList * node;
-  friend class driver_Class;
+  LinkedList * node;
+  friend class Driver_Class;
 };
-class driver_Class {
+class Driver_Class {
   public:
     char * error = "error\n";
-  linkedList * createNode(int);
-  void printList(linkedList * temp) {
+  LinkedList * createNode(int);
+  void printList(LinkedList * temp) {
     while (temp != NULL) {
       cout << temp -> data << endl;
       temp = temp -> node;
     }
   }
-  inline linkedList * setNode(linkedList * temp) {
+  inline LinkedList * setNode(LinkedList * temp) {
     return temp -> node;
   }
-  void setNumber(linkedList * node, int n, driver_Class & d1) {
+LinkedList *DeleteFirstNode(LinkedList *start){
+    LinkedList *tempStart;
+    tempStart=start;
+    if(start!=NULL){
+       start=start->node;
+       free(tempStart); 
+       return start;
+    }
+    
+    else{
+        cout<<"The list is empty"<<endl;
+    }
+}  
+void DeleteSpecificNode(int data,LinkedList *start){
+    LinkedList *tempStart,*tempTempStart;
+    int count = 0;
+    tempStart = start;
+     if(start->data==data){
+       start=start->node;
+       free(tempStart); 
+       return;
+    }
+    tempTempStart= start;
+    while(tempStart->data!=data){
+        tempStart = tempStart->node;
+        if(count!=0){
+            tempTempStart=tempTempStart->node;
+        }
+        count++;
+    }
+    tempStart=tempStart->node;
+    tempTempStart->node=tempStart;
+   // free(tempStart);
+}
+  void setNumber(LinkedList * node, int n, Driver_Class & d1) {
     for (int i = 1; i <= n; i++) {
       node -> node = d1.createNode(i * 12);
       node = d1.setNode(node);
     }
   }
 };
-linkedList * driver_Class::createNode(int data1) {
-  linkedList * temp;
+LinkedList * Driver_Class::createNode(int data1) {
+  LinkedList * temp;
   try {
-    temp = new linkedList;
+    temp = new LinkedList;
     if (temp) {
       temp -> data = data1;
       temp -> node = NULL;
@@ -44,11 +78,24 @@ linkedList * driver_Class::createNode(int data1) {
 }
 
 int main() {
-  driver_Class d1;
-  linkedList * node, * start, * temp;
-  node = d1.createNode(11);
+  Driver_Class *d1;
+  LinkedList * node, * start, * temp;
+  node = d1->createNode(11);
   start = node;
-  d1.setNumber(node, 10, d1);
+  d1->setNumber(node, 10, *d1);
   temp = start;
-  d1.printList(temp);
+  d1->printList(temp);
+  system("pause");
+  system("cls");
+
+  start=d1->DeleteFirstNode(start);
+  temp=start;
+  d1->printList(temp);
+  system("pause");
+  system("cls");
+
+   d1->DeleteSpecificNode(72,temp);
+   d1->printList(temp);
+   system("pause");
+
 }
